9d0ba21a52e54de59f5f9db02e2d99280021010b
santiago <santiagoox10@gmail.com>
2024-04-13 22:13:07 -0500

    se subio el archivo

---
+++ EstructurasDeControl.js
@@ -0,0 +1,107 @@
+//declaramos el prompt como costante para que funcione el NODEJS
+const prompt = require('prompt-sync')();
+
+//funcion que declara el minimo y el maximo de el numero
+function generarNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//funcion para jugar a adivinar un numero aleatorio generado 
+function adivinarNumero() {
+  const numeroAleatorio = generarNumeroAleatorio(0, 99);//genera un numero aleatorio entre 0 y 99 
+  let intentos = 0;//el contador de intetos realizado
+
+  while (true) { // bucle hasta adivinar el numero 
+    const numeroUsuario = parseInt(prompt("Adivina el número (entre 0 y 99):"));
+
+    if (isNaN(numeroUsuario)) {
+      console.log("Por favor ingresa un número válido.");//si la entrada es un numero manda un mensaje
+    } else if (numeroUsuario <= 0 || numeroUsuario >= 100) {
+      console.log("El número debe estar entre 0 y 99.");//verifica si la entrada esta fuera del rango
+    } else if (numeroUsuario < numeroAleatorio) {
+      console.log("El número es demasiado bajo. Inténtalo de nuevo.");// si la entrada es menor al numero aleatorio
+    } else if (numeroUsuario > numeroAleatorio) {
+      console.log("El número es demasiado alto. Inténtalo de nuevo.");// si la entrada es mayor al numero aleatorio
+    } else {
+      console.log(`¡Felicidades! Has adivinado el número correcto en ${intentos} intentos.`);// si el usuario adivina el numero aleatorio
+      break;//finaliza el bucle 
+    }
+
+    intentos++;//sube el contador de intentos
+  }
+}
+//funcion para calcular el factorial de un numero usando un ciclo for
+function calcularFactorialFor(numero) {
+  let factorial = 1;
+
+  // verifica si el numero es negativo y si lo es manda un mensaje 
+  if (numero < 0 )
+  return "Ingrese un valor igual o mayor a 0";
+  else{
+    // calcula el factorial de un numero usando el ciclo for 
+    for (let i = 1; i <= numero; i++) {
+      factorial *= i;
+    }
+  }
+  
+  //manda el resultado de factorial
+  return factorial;
+}
+
+//funcion para contar las vocales en una frase dada
+function contarVocales(frase) {
+  frase = frase.toLowerCase();// convierte una frase a minisculas para facilitar la comparacion
+  const vocales = ['a', 'e', 'i', 'o', 'u'];//array de vocales 
+  const vocalesEncontradas = {};//almacena el conteo de vocales 
+
+  //pone el contador en cada vocal en cero
+  for (let vocal of vocales) {
+    vocalesEncontradas[vocal] = 0;
+  }
+
+  //busca en la frase y cuenta las vocales 
+  for (let caracter of frase) {
+    if (vocales.includes(caracter)) {
+      vocalesEncontradas[caracter]++;
+    }
+  }
+
+  //imprime el conteo de vocales 
+  console.log("Vocales encontradas y su cantidad:");
+  for (let vocal of vocales) {
+    console.log(`${vocal}: ${vocalesEncontradas[vocal]}`);
+  }
+}
+
+//menu principal
+let opcion;
+do {
+  console.log("");
+  console.log("MENU:");
+  console.log("1. Adivinar el número");
+  console.log("2. Calculadora de Factoriales");
+  console.log("3. Contador de vocales");
+  console.log("4. Salir");
+  opcion = parseInt(prompt("Seleccione una opción: "));//pide al usuario una opcion en numeros 
+
+  switch (opcion) {
+    case 1:
+      adivinarNumero();//llama la funcion
+      break;
+    case 2:
+      const numeroUsuarioFactorial = parseInt(prompt("Ingrese un número para calcular su factorial:"));
+      const factorialFor = calcularFactorialFor(numeroUsuarioFactorial);
+      console.log(`El factorial de ${numeroUsuarioFactorial} usando ciclo for es: ${factorialFor}`);
+      break;
+    case 3:
+      const fraseUsuarioVocales = prompt("Ingresa una frase para contar las vocales:");
+      contarVocales(fraseUsuarioVocales);//llama la funcion
+      break;
+    case 4:
+      console.log("Hasta pronto");//mensaje al salir del menu
+      break;
+    default:
+      console.log("Ingrese una opción válida");//mensaje si la opcion no es valida
+      break;
+  }
+} while (opcion !== 4);//bucle hasta que opcion sea 4
---
+++ Listas.js
@@ -0,0 +1,61 @@
+//esta funcion es para buscar el numero minimo y maximo de una lista 
+function encontrarExtremos(lista) {
+  //aqui verfica si la lista esta vacia 
+  if (lista.length === 0) {
+    return null; // vulve a null si la lista está vacía
+  }
+//inicia las variables minimo y maximo con el primir elemento de la lista 
+  let minimo = lista[0];
+  let maximo = lista[0];
+
+  // lee la lista para encontar el minimo y el maximo
+  for (let i = 1; i < lista.length; i++) {
+    if  (lista[i] < minimo) {
+      minimo = lista[i];//cambia el minimo si se encuentra un valor menor
+    }
+    if (lista[i] > maximo) {
+      maximo = lista[i];//cambia el maximo si se encuentra un valor mayor
+    }
+  }
+
+  //manda un objeto con las caracteristicas minimo y maximo
+  return { minimo, maximo };
+}
+
+// funcion para cocatenar y ordenar la lista resultante 
+function concatenarYOrdenar(lista1, lista2) {
+  // concatena las dos listas y pone el resultado en listaConcatenada
+  const listaConcatenada = lista1.concat(lista2);
+  listaConcatenada.sort((a, b) => a - b); // Ordena de menor a mayor
+  // vuelve a mandar la lista concatenada y ordenada
+  return listaConcatenada;
+}
+
+// Funcion para inveertir el orde de los elementos en una lista 
+function invertirLista(lista) {
+  //aqui se invierte la lista 
+  return lista.reverse();
+}
+
+//aqui esta las listas para los ejercicios 
+const numeros = [112, 1223, 234, 2345, 1244, 12233];
+const extremos = encontrarExtremos(numeros);
+console.log("la lista es ", numeros);
+console.log("");
+console.log("Número más pequeño:", extremos.minimo);
+console.log("Número más grande:", extremos.maximo);
+
+const lista1 = [15, 65, 160];
+const lista2 = [13, 12, 6];
+const listaConcatenadaOrdenada = concatenarYOrdenar(lista1, lista2);
+console.log("");
+console.log("lista 1:", lista1);
+console.log("lista 2:", lista2);
+console.log("");
+console.log("Lista concatenada y ordenada:", listaConcatenadaOrdenada);
+
+const listaOriginal = [154, 235, 345, 456, 536];
+const listaInvertida = invertirLista(listaOriginal);
+console.log("");
+console.log("Lista original:", listaOriginal);
+console.log("Lista invertida:", listaInvertida);

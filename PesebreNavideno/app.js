cd5d550ff753b901291e9530e2af528f86a16831
santiago <santiagoox10@gmail.com>
2024-05-08 22:22:55 -0500

    pesebre copia 1

---
+++ PesebreNavideno/app.js
@@ -0,0 +1,191 @@
+function aleatorio() {
+  let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  lista.sort(() => Math.random() - 0.5);
+  console.log(lista);
+
+  document.getElementsByClassName("carta-1")[0].style.order = lista[0];
+  document.getElementsByClassName("carta-2")[0].style.order = lista[1];
+  document.getElementsByClassName("carta-3")[0].style.order = lista[2];
+  document.getElementsByClassName("carta-4")[0].style.order = lista[3];
+  document.getElementsByClassName("carta-5")[0].style.order = lista[4];
+  document.getElementsByClassName("carta-6")[0].style.order = lista[5];
+  document.getElementsByClassName("carta-7")[0].style.order = lista[6];
+  document.getElementsByClassName("carta-8")[0].style.order = lista[7];
+  document.getElementsByClassName("carta-9")[0].style.order = lista[8];
+  document.getElementsByClassName("carta-10")[0].style.order = lista[9];
+  document.getElementsByClassName("carta-11")[0].style.order = lista[10];
+  document.getElementsByClassName("carta-12")[0].style.order = lista[11];
+  document.getElementsByClassName("carta-13")[0].style.order = lista[12];
+  document.getElementsByClassName("carta-14")[0].style.order = lista[13];
+  document.getElementsByClassName("carta-15")[0].style.order = lista[14];
+  document.getElementsByClassName("carta-16")[0].style.order = lista[15];
+}
+aleatorio();
+document.addEventListener("DOMContentLoaded", function () {
+  const cartas = document.querySelectorAll(".carta");
+  let volteadas = 0;
+  let primeraCarta = null;
+  let segundaCarta = null;
+  let parejasEncontradas = 0;
+  let bloquearClicks = false;
+  const totalCartas = cartas.length / 2;
+  let audioCorrecta = new Audio("Audio/correcta.mp3");
+  let audioIncorrecta = new Audio("Audio/incorrecta.mp3");
+  let audioGaspar = new Audio("Audio/gaspar.mp3");
+  let audioBaltazar = new Audio("Audio/baltazar.mp3");
+  let audioJesus = new Audio("Audio/jesus.mp3");
+  let audioJose = new Audio("Audio/jose.mp3");
+  let audioMaria = new Audio("Audio/maria.mp3");
+  let audioMelchor = new Audio("Audio/melchor.mp3");
+  let audioMula = new Audio("Audio/mula.mp3");
+  let audioPastor = new Audio("Audio/pastor.mp3");
+  let audioFinal = new Audio("Audio/final.mp3");
+  let contenedorPesebre = document.getElementById("contenedor-pesebre");
+  let CartonMaria = document.querySelector(".maria-png");
+  let CartonJesus = document.querySelector(".jesus-png");
+  let CartonJose = document.querySelector(".jose-png");
+  let CartonGaspar = document.querySelector(".gaspar-png");
+  let CartonBaltazar = document.querySelector(".baltazar-png");
+  let CartonMula = document.querySelector(".mula-buey-png");
+  let Cartonmelchor = document.querySelector(".melchor-png");
+  let CartonPastor = document.querySelector(".pastor-png");
+
+  cartas.forEach(function (carta) {
+    carta.addEventListener("click", function () {
+      if (bloquearClicks) return;
+
+      const cartaInner = this.querySelector(".carta-inner");
+      const flipped = cartaInner.getAttribute("data-flipped") === "true";
+      const encontrado = cartaInner.getAttribute("data-encontrado") === "true";
+      const valorCarta = carta.getAttribute("data-valor"); // Obtener el valor de data-valor de la carta
+
+      if (!flipped && !encontrado) {
+        cartaInner.style.transform = "rotateY(180deg)";
+        cartaInner.setAttribute("data-flipped", "true");
+        volteadas++;
+
+        if (volteadas === 1) {
+          primeraCarta = carta;
+        } else if (volteadas === 2) {
+          segundaCarta = carta;
+
+          bloquearClicks = true; // Bloquear los clics mientras se evalúan las cartas
+
+          // Espera un momento antes de evaluar si las cartas son iguales
+          setTimeout(function () {
+            if (
+              primeraCarta.getAttribute("data-valor") === segundaCarta.getAttribute("data-valor")) {
+              console.log("Encontraste", valorCarta);
+              primeraCarta.setAttribute("data-encontrado", "true");
+              segundaCarta.setAttribute("data-encontrado", "true");
+
+              audioCorrecta.play();
+
+              if (parejasEncontradas === totalCartas) {
+                console.log("ganado");
+                audioFinal.play();
+                parejasEncontradas++;
+              }
+              if (valorCarta == 1) {
+                contenedorPesebre.classList.remove("hidden");
+                CartonMaria.classList.toggle("caida");
+                audioMaria.play();
+                setTimeout(() => {
+                  CartonMaria.classList.toggle("hidden");
+                }, 2900);
+              } else if (valorCarta == 2) {
+                contenedorPesebre.classList.remove("hidden");
+                CartonJose.classList.toggle("caida");
+                audioJose.play();
+                setTimeout(() => {
+                  CartonJose.classList.toggle("hidden");
+                }, 2900);
+              } else if (valorCarta == 3) {
+                contenedorPesebre.classList.remove("hidden");
+                CartonJesus.classList.toggle("caida");
+                audioJesus.play();
+                setTimeout(() => {
+                  CartonJesus.classList.toggle("hidden");
+                }, 2900);
+              } else if (valorCarta == 4) {
+                contenedorPesebre.classList.remove("hidden");
+                Cartonmelchor.classList.toggle("caida");
+                audioMelchor.play();
+                setTimeout(() => {
+                  Cartonmelchor.classList.toggle("hidden");
+                }, 2900);
+              } else if (valorCarta == 5) {
+                contenedorPesebre.classList.remove("hidden");
+                CartonGaspar.classList.toggle("caida");
+                audioGaspar.play();
+                setTimeout(() => {
+                  CartonGaspar.classList.toggle("hidden");
+                }, 2900);
+              } else if (valorCarta == 6) {
+                contenedorPesebre.classList.remove("hidden");
+                CartonBaltazar.classList.toggle("caida");
+                audioBaltazar.play();
+                setTimeout(() => {
+                  CartonBaltazar.classList.toggle("hidden");
+                }, 2900);
+              } else if (valorCarta == 7) {
+                contenedorPesebre.classList.remove("hidden");
+                CartonPastor.classList.toggle("caida");
+                audioPastor.play();
+                setTimeout(() => {
+                  CartonPastor.classList.toggle("hidden");
+                }, 2900);
+              } else if (valorCarta == 8) {
+                contenedorPesebre.classList.remove("hidden");
+                CartonMula.classList.toggle("caida");
+                audioMula.play();
+                setTimeout(() => {
+                  CartonMula.classList.toggle("hidden");
+                }, 2900);
+              }
+            } else {
+              // Las cartas no coinciden, gíralas de vuelta
+              primeraCarta.querySelector(".carta-inner").style.transform =
+                "rotateY(0deg)";
+              segundaCarta.querySelector(".carta-inner").style.transform =
+                "rotateY(0deg)";
+              primeraCarta
+                .querySelector(".carta-inner")
+                .setAttribute("data-flipped", "false");
+              segundaCarta
+                .querySelector(".carta-inner")
+                .setAttribute("data-flipped", "false");
+              console.log("no coinciden");
+              audioIncorrecta.play();
+            }
+
+            volteadas = 0; // Reinicia el contador de cartas volteadas
+            bloquearClicks = false; // Desbloquear los clics después de evaluar las cartas
+          }, 700); // Tiempo en milisegundos antes de evaluar (1 segundo)
+        }
+      }
+    });
+  });
+});
+
+let button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  let welcome2 = document.getElementById("welcome2");
+  welcome2.parentNode.removeChild(welcome2);
+
+  let welcome1 = document.getElementById("welcome1");
+  welcome1.parentNode.removeChild(welcome1);
+
+  let audioIntro = document.createElement("audio");
+  audioIntro.src = "Audio/intro.mp3";
+  audioIntro.autoplay = true;
+  document.body.appendChild(audioIntro);
+});
+let button1 = document.getElementById("continuar");
+
+button1.addEventListener("click", () => {
+  let pesebre = document.getElementById("contenedor-pesebre");
+  pesebre.classList.toggle("hidden");
+  document.body.preventDefault();
+});

62f230b1a0a455eebf4935c05eda4bd3af53af1e
santiago-o-o <166684475+santiago-o-o@users.noreply.github.com>
2026-07-29 20:25:11 -0500

    Delete history.service.spec.ts

--- history.service.spec.ts
+++
@@ -1,9 +0,0 @@
-<div class="homecomonent">
-  <div class="leftSection">
-    <app-history></app-history>
-  </div>
-  <div class="rightSection">
-    <app-search></app-search>
-    <app-grid-gifs/>
-  </div>
-</div>
